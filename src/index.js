32366fc30cbb4d629ee387153511a230443c34f2
eray <eraydemir1@yandex.com>
2018-11-28 15:12:52 +0300

    navbar edited

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './assets/css/style.css'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faStroopwafel } from '@fortawesome/free-solid-svg-icons'
 import Home from './router/Home/index';
 import Blog from './router/Blog/index';
 import About from './router/About/index';
@@ -9,7 +12,13 @@ import Contact from './router/Contact/index';
 import Projects from './router/Projects/index';
 import { Router, Route, browserHistory} from 'react-router'
 import * as serviceWorker from './serviceWorker';
+library.add(faStroopwafel)
 
+export const Food = () => (
+  <div>
+    Favorite Food: <FontAwesomeIcon icon="stroopwafel" />
+  </div>
+);
 ReactDOM.render(
     <Router history = {browserHistory}>
         <Route path = "/" component = {Home} />
